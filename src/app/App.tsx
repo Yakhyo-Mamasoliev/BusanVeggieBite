bb48e95a143f5d03576b1e4d9139d76239fe7903
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-07-10 17:49:30 +0900

    feat: integration rooter and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
-import { HomePage } from "./screens/homePage";
-import { ProductsPage } from "./screens/productsPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { UserPage } from "./screens/userPage";
-import { HomeNavbar } from "../components/headers/HomeNavbar";
-import { OtherNavbar } from "../components/headers/OtherNavbar";
-import { Footer } from "../components/footer";
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import HomeNavbar from "../components/headers/HomeNavbar";
+import OtherNavbar from "../components/headers/OtherNavbar";
+import Footer from "../components/footer";
+import HelpPage from "./screens/helpPage";
 import "../css/app.css";
 import "../css/navbar.css";
-import { HelpPage } from "./screens/helpPage";
+import "../css/footer.css";
+
 function App() {
   const location = useLocation();
   console.log("location:", location);
@@ -28,7 +30,7 @@ function App() {
         <Route path="/member-page">
           <UserPage />
         </Route>
-        <Route path="/help-page">
+        <Route path="/help">
           <HelpPage />
         </Route>
         <Route path="/">
--- src/components/headers/OtherNavbar.tsx
+++ src/components/headers/OtherNavbar.tsx
@@ -1,7 +1,8 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import Basket from "./Basket";
 
-export function OtherNavbar() {
+export default function OtherNavbar() {
   const authMember = null;
   return (
     <div className="home-navbar">
@@ -35,12 +36,14 @@ export function OtherNavbar() {
                 </NavLink>
               </Box>
             ) : null}
+            
             <Box className={"hover-line"}>
               <NavLink to="/help" activeClassName="underline">
                 Help
               </NavLink>
             </Box>
-            {/* BASKET */}
+            
+            <Basket />
 
             {!authMember ? (
               <Box>
