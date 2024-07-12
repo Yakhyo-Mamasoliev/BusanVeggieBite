9cea7c2f763c4fd0075fa9c28d8504d125ab8ed8
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-07-12 09:29:40 +0900

    fix: modifystatistics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,6 +5,7 @@ import NewDishes from "./NewDishes";
 import Advertisiement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
+import "../../../css/home.css";
 
 export default function HomePage() {
   return (
