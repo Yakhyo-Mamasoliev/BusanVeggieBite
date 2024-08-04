9d29cf2776e4333e8ee2e613daa87af97b7607dc
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-08-04 14:37:10 +0500

    feat: develop ProductsPAge slice and selector

--- src/lib/types/screen.ts
+++ src/lib/types/screen.ts
@@ -1,16 +1,28 @@
+/* eslint-disable @typescript-eslint/no-redeclare */
 import { Member } from "./member";
 import { Product } from "./product";
+import ProductsPage from "../../app/screens/productsPage/index";
 
 /* React APP STATE */
 
 export interface AppRootState {
   homePage: HomePageState;
+  productsPage: ProductsPageState;
 }
 
-/* HOMEPAGE */
+//* HOMEPAGE */
 
 export interface HomePageState {
   popularDishes: Product[];
   newDishes: Product[];
   topUsers: Member[];
 }
+
+//** PRODUCTS PAGE HERE */
+export interface ProductsPageState {
+  restaurant: Member | null;
+  chosenProduct: Product | null;
+  products: Product[];
+}
+
+//** ORDERS PAGE HERE **
